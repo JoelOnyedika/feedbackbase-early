b1d55bc44cd75fa40e7f7db619945ab206306607
JoelOnyedika <joelonyedikaepic@gmail.com>
2024-09-19 20:30:19 +0100

    test and img config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   //reactStrictMode: true,
   images: {
     domains: ['api.dicebear.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '/6.x/**',
+      },
+    ],
   },
 };
 
